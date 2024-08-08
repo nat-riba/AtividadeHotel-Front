9cdf4ed32c66c0175742354e695dafd25323c5e0
Nathália Ribeiro <nat_riba@hotmail.com>
2024-08-08 13:33:18 -0300

    pages

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Clientes from "./pages/Clientes";
+import NovoCliente from "./pages/NovoCliente";
+import EditarCliente from "./pages/EditarCliente";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/clientes/novo" element={<NovoCliente />} />
+          <Route path="/clientes/editar/:id" element={<EditarCliente />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster position="bottom-right" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header w-100 px-3 py-2">
+      <nav className="container d-flex justify-content-between align-items-center">
+        <Link to="/">
+          <img src={logo} alt="imagem hotel" /> 
+        </Link>
+        <div className="d-flex gap-5">
+          <Link to="/clientes">Clientes</Link>
+          <Link to="/pets">Pets</Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
