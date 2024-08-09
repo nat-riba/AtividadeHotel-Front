7474d3e77d43e088dc33bd64aa3667eb23a6190e
Adriel Marx <155272647+AdrielMarx@users.noreply.github.com>
2024-08-09 14:39:40 -0300

    :bug:

--- atividadehotel-front/src/App.jsx
+++ atividadehotel-front/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <Route path="/clientes/novo" element={<NovoCliente />} />
           <Route path="/clientes/editar/:id" element={<EditarCliente />} />
           <Route path="/reserva" element={<Reserva />} />
-          <Route path="/resera/novo" element={<AdicionarReserva />}/>
+          <Route path="/reserva/novo" element={<AdicionarReservas />}/>
           <Route path="/quartos" element={<Quarto />} />
           <Route path="/pagamentos" element={<Pagamentos />} />
         </Routes>
